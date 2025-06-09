ec84911abde922c4876b67b10c04c39f9519a31e
Firas Dhaouadi <firasdhaouadi@edtrust.tn>
2025-06-09 14:16:30 +0100

    update

--- startup.sh
+++ startup.sh
@@ -4,11 +4,14 @@
 # Start the first Java process in background
 
 cd /tracking
-java -jar trakingServer_out.jar server_40028.conf 1> /srv/www/tracking/log40028.log 2> /srv/www/tracking/log40028.log &
+java -jar trakingServer.jar server_40028.conf 1> /srv/www/tracking/log40028.log 2> /srv/www/tracking/log40028.log &
 
 # Start the second Java process in background
 cd /rt_mqtt
-java -XX:+UseSerialGC -jar CollectorRTMQTT_out.jar 1>> /srv/www/tracking/log_succcess_rtm.log 2>> /srv/www/tracking/log_err_rtm.log &
+java -XX:+UseSerialGC -jar CollectorRTMQTT.jar 1>> /srv/www/tracking/log_success_rtm.log 2>> /srv/www/tracking/log_err_rtm.log &
+
+cd /path
+java -XX:+UseSerialGC -jar PathCollector.jar 1>> /srv/www/tracking/log_success_path.log 2>> /srv/www/tracking/log_err_path.log &
 
 # Keep the container running (important!)
 tail -f /dev/null
